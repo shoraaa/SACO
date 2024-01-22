8a4792c4885d709af2025eab665b722a401746ef
shoraaa <shoraaa@protonmail.com>
2024-01-22 23:08:24 +0700

    update faco.cpp and progargs.h

--- src/progargs.h
+++ src/progargs.h
@@ -14,13 +14,13 @@ struct ProgramOptions {
     // When looking for a next node to visit it may happen that all of the
     // nodes on the candidates list were visited -- in such case we choose
     // one of the nodes from a "backup" list
-    uint32_t backup_list_size_ = 64;
+    uint32_t backup_list_size_ = 128;
 
     // Relative importance of heuristic information, i.e. distances between
     // nodes
     double beta_ = 5;
 
-    uint32_t cand_list_size_ = 16;
+    uint32_t cand_list_size_ = 32;
 
     std::string id_ = "default";  // Id of the comp. experiment
 
