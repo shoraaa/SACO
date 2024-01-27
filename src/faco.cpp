1fefb479f87a5945b2a0987d20c0c0262dff7e8f
shoraaa <shoraaa@protonmail.com>
2024-01-28 02:31:53 +0700

    update faco.cpp

--- src/faco.cpp
+++ src/faco.cpp
@@ -758,9 +758,6 @@ run_focused_aco(const ProblemInstance &problem,
             #pragma omp barrier
 
             model.evaporate_pheromone();
-            if (cur_i < 2) {
-                iteration_bests[cur_i++] = *iteration_best;
-            }
 
             // TODO:
             #pragma omp master
