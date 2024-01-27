0902d13772bac3fab23e8c7fd25d273438bdd888
shoraaa <shoraaa@protonmail.com>
2024-01-27 16:09:11 +0700

    update faco.cpp

--- src/faco.cpp
+++ src/faco.cpp
@@ -771,8 +771,8 @@ run_focused_aco(const ProblemInstance &problem,
         }
     }
     comp_log("pher_deposition_time", pher_deposition_time);
-    cout << "MAXIMUM EDGES ADDED: " << max_min_new_edges << '\n';
-    cout << "AVERAGE: " << (double) max_min_new_edges / problem.dimension_ << '\n';
+    cout << "MAXIMUM EDGES ADDED: " << max_min_new_edges << endl;
+    cout << "AVERAGE OF EDGES: " << (double) max_min_new_edges / problem.dimension_ << endl;
 
     return unique_ptr<Solution>(dynamic_cast<Solution*>(best_ant.release()));
 }
