d412939c0ef0269d53e8a6e22f0001aa9178ae15
shoraaa <shoraaa@protonmail.com>
2024-01-30 09:13:19 +0700

    update faco.cpp

--- src/faco.cpp
+++ src/faco.cpp
@@ -592,6 +592,7 @@ run_focused_aco(const ProblemInstance &problem,
                     auto best_cost = recent_sol.front().cost_;
                     auto best_i = 0;
                     for (size_t i = 0; i < recent_sol.size(); ++i) {
+                        auto cost = recent_sol[i].cost_ * get_rng().next_float();
                         if (recent_sol[i].cost_ < best_cost) {
                             best_cost = recent_sol[i].cost_;
                             best_i = i;
