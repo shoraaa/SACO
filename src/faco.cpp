88dcd230bb7d975cc2f689f671f8b8d80653c89d
shoraaa <shoraaa@protonmail.com>
2024-02-02 23:49:08 +0700

    update faco.cpp

--- src/faco.cpp
+++ src/faco.cpp
@@ -509,7 +509,7 @@ run_focused_aco(const ProblemInstance &problem,
 
                 uint32_t k = 1, u = start_node;
                 ant.update(source_solution->route_, source_solution->cost_);
-                ant.visited_bitmask_.set_bit(node);
+                ant.visited_bitmask_.set_bit(u);
                 while (k < dimension && new_edges < target_new_edges) {
                     auto v = select_next_node(pheromone, heuristic,
                                                  problem.get_nearest_neighbors(curr, cl_size),
