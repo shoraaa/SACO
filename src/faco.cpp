633cdc1d443e016c14360a59e0ea17d69e9fd11f
shoraaa <shoraaa@protonmail.com>
2024-02-04 00:36:42 +0700

    update faco.cpp and progargs.h

--- src/faco.cpp
+++ src/faco.cpp
@@ -639,7 +639,6 @@ run_focused_aco(const ProblemInstance &problem,
             #pragma omp master
             {
                 if (iteration % 1000 == 0) {
-                    gbest_as_source_prob_ += 0.1;
                     auto error = problem.calc_relative_error(best_ant->cost_);
                     best_cost_trace.add({ best_ant->cost_, error }, iteration, main_timer());
                 }
