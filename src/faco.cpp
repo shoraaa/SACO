6f9bea44d83df373b78889ed5d2b35f0a0e7b418
shoraaa <shoraaa@protonmail.com>
2024-02-05 22:23:39 +0700

    update faco.cpp

--- src/faco.cpp
+++ src/faco.cpp
@@ -364,7 +364,7 @@ public:
     }
 
     void evaporate_pheromone() {
-        get_pheromone().evaporate(1 - rho_, trail_limits_.min_, rho_ * trail_limits_.min_);
+        get_pheromone().evaporate(1 - rho_, trail_limits_.min_, -rho_ * trail_limits_.min_);
     }
 
     decltype(auto) get_pheromone() {
